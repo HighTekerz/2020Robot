090f0926a3b0a98186e7cc2f90053a92cac55620
Levitaph <Iminecraft01@gmail.com>
2020-03-04 16:29:58 -0800

    PIDTuner fully functionalized

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,54 +7,80 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.L;
 import frc.robot.MechanoidAtlas;
 
 public class Shooter extends SubsystemBase {
-  
-  // Unknown Numbers
-  private TalonFX leftFlywheel = new TalonFX(MechanoidAtlas.leftShooterMotor);
-  private TalonFX rightFlywheel = new TalonFX(MechanoidAtlas.rightShooterMotor);
 
-  public static Shooter shooter;
+	// Unknown Numbers
+	private TalonFX leftFlywheel = new TalonFX(MechanoidAtlas.leftShooterMotor),
+			rightFlywheel = new TalonFX(MechanoidAtlas.rightShooterMotor);
 
-  public static Shooter getInstance(){
-    if (shooter == null){
-      shooter = new Shooter();
-    }
-    return shooter;
-  }
+	public static final double POSITION1 = 30, 
+											POSITION2 = 30, 
+											POSITION3 = 30;
 
-  double p, 
-             i, 
-             d, 
-              loopLengthInSeconds;
+	public static Shooter shooter;
 
-  private final PIDController pIDLoop = new PIDController(p,i,d, loopLengthInSeconds);
+	private boolean spinning = false;
 
-  /**
-   * Creates a new Shooter.
-   */
-  private Shooter() {
-  }
+	public static Shooter getInstance() {
+		if (shooter == null) {
+			shooter = new Shooter();
+		}
+		return shooter;
+	}
 
-  public void setSetpoint(double setpoint){
-    pIDLoop.setSetpoint(setpoint);
-  }
+	
+	private double p, i, d, fF = 0.0, loopLengthInSeconds = .02;
 
-  public void stopPid(){
-    leftFlywheel.set(TalonFXControlMode.Disabled, 0);
-    rightFlywheel.set(TalonFXControlMode.Disabled, 0);
-  }
+	public final PIDController pIDLoop = new PIDController(p, i, d, loopLengthInSeconds);
 
-  
+	/**
+	 * Creates a new Shooter.
+	 */
+	private Shooter() {
+		leftFlywheel.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+		pIDLoop.enableContinuousInput(-10000, 10000);
+	}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	public void setSetpoint(double setpoint) {
+		pIDLoop.setSetpoint(setpoint);
+		spinning = true;
+	}
+
+	public void stopPID() {
+		spinning = false;
+		leftFlywheel.set(TalonFXControlMode.Disabled, 0);
+	}
+
+	public void runPID() {
+		double pIDOutput = pIDLoop.calculate(leftFlywheel.getSelectedSensorVelocity());
+		leftFlywheel.set(ControlMode.PercentOutput, pIDOutput + fF);
+		L.ogSD("PID Output", pIDOutput);
+		L.ogSD("LeftFly Velocity", leftFlywheel.getSelectedSensorVelocity());
+	}
+
+	public double getFF(){
+		return fF;
+	}
+
+	public void setFF(double newValue){
+		fF = newValue;
+	}
+
+
+	@Override
+	public void periodic() {
+		if (spinning){
+			runPID();
+		}	
+	}
 }
